4c5fea128d1020d9102d8d4a732d1bb948feb6f7
vektor <vektor@vektor-PC>
2016-11-14 18:12:47 -0700

    Begin Deserializer class.

---
+++ src/cpsc501_a3/Deserializer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+       
+package cpsc501_a3;
+
+import org.jdom2.Document;
+
+/**
+ *
+ * @author vektor
+ */
+public class Deserializer {
+    
+    public Object deserialize(Document document) {
+        
+    }
+    
+}
