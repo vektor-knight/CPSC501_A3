4eda461e34e09b10111c979aaf505501bf09bd17
vektor <vektor@vektor-PC>
2016-11-14 18:33:38 -0700

    Finish createInstances(..). Check if tests are generated for it.

--- src/cpsc501_a3/Deserializer.java
+++ src/cpsc501_a3/Deserializer.java
@@ -6,10 +6,14 @@
        
 package cpsc501_a3;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.jdom2.Document;
+import org.jdom2.Element;
 
 /**
  *
@@ -29,8 +33,23 @@ public class Deserializer {
         return table.get("0");  
     }
 
-    private static void createInstances(Map table, List<?> objectList) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void createInstances(Map table, List<?> objectList) throws Exception{
+        for (int i = 0; i < objectList.size(); i++) {
+            Element objectElement = (Element) objectList.get(i);
+            Class c = Class.forName(objectElement.getAttributeValue("class"));
+            Object instance = null;
+            
+            if (!c.isArray()) {
+                Constructor x = c.getDeclaredConstructor(null);
+                if (!Modifier.isPublic(x.getModifiers())) {
+                    x.setAccessible(true);
+                }
+                instance = x.newInstance(null);
+            } else {
+                instance = Array.newInstance(c.getComponentType(), Integer.parseInt(objectElement.getAttributeValue("length")));
+            }
+            table.put(objectElement.getAttributeValue("id"), instance);
+        }
     }
 
     private static void assignFieldValues(Map table, List<?> objectList) {
