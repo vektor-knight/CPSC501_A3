56b3428714001046ed153bf2de9c023610a5fc92
vektor <vektor@vektor-PC>
2016-11-16 01:29:38 -0700

    Refactor: changing signature (Object[] -> Boolean)

---
+++ src/Sender/ObjectBundle/ReferenceArray.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sender.ObjectBundle;
+
+/**
+ *
+ * @author vektor
+ */
+public class ReferenceArray {
+    
+    public Object[] x;
+    
+    public ReferenceArray() {
+        
+    }
+    
+    public ReferenceArray(Object[] i) {
+        x = i;
+    }
+    
+}
