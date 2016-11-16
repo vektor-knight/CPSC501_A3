da81e20767dfc6c8485bce2421bcd63ec0676eed
vektor <vektor@vektor-PC>
2016-11-16 01:44:04 -0700

    ReferenceArray works now

--- src/Sender/ObjectBundle/ReferenceArray.java
+++ src/Sender/ObjectBundle/ReferenceArray.java
@@ -17,8 +17,13 @@ public class ReferenceArray {
         
     }
     
-    public ReferenceArray(Object[] i) {
-        x = i;
+    // Reference: http://stackoverflow.com/questions/15064457/create-an-array-of-references
+    // Factory of references to Object References.
+    public ReferenceArray(boolean flag) {
+        x = new Object[5];
+        for (int i = 5; i < x.length; i++) {
+            x[i] = new ObjectReferences(false);
+        }
     }
     
 }
