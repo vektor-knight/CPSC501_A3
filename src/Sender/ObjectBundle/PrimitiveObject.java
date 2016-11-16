c07a587188d35786238020d0cec2e159c16d21de
vektor <vektor@vektor-PC>
2016-11-15 18:09:22 -0700

    Input handler for PrimitiveObject done.

--- src/Sender/ObjectBundle/PrimitiveObject.java
+++ src/Sender/ObjectBundle/PrimitiveObject.java
@@ -5,8 +5,11 @@
  */
 package Sender.ObjectBundle;
 
+import java.util.Scanner;
+
 /**
- *
+ * Factory method pattern application from:
+ * https://github.com/add-em/ObjectSerializer/blob/master/ObjectSimple.java
  * @author vektor
  */
 public class PrimitiveObject {
@@ -18,6 +21,25 @@ public class PrimitiveObject {
     
     public PrimitiveObject() {
     
+    }
+    
+    /**
+     * When the flag is set to true in the ObjectCreator, the user
+     * will be prompted with this constructor's body of statements.
+     * @param flag 
+     */
+    public PrimitiveObject(boolean flag) {
+        System.out.println("Set the object's fields:");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("\n Enter integer: ");
+        integerField = scan.nextInt();
+        
+        System.out.println("Enter double: ");
+        doubleField = scan.nextDouble();
+        
+        System.out.println("Enter character: ");
+        charField = scan.next().charAt(0);
     }
     
     // To be used for unit testing
