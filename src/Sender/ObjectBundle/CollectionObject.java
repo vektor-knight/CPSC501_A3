a55c7294f0cb692442c724a7b05c3934a4b652f5
vektor <vektor@vektor-PC>
2016-11-16 01:28:04 -0700

    Refactor: removing unused classes

---
+++ src/Sender/ObjectBundle/CollectionObject.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sender.ObjectBundle;
+
+// Vector wasn't working, so adapted:
+// https://github.com/stevarms/CPSC501A3/blob/master/src/collectionObject.java
+// Instead of directly appending the instance variable to a 
+import java.util.ArrayList;
+import java.util.Scanner;
+// import java.util.Vector;
+
+/**
+ *
+ * @author vektor
+ */
+public class CollectionObject {
+    
+    public ArrayList a;
+   // int size;
+    
+    public CollectionObject() {
+        
+    }
+    
+    public CollectionObject(boolean flag){
+        ArrayList a1 = a;
+    }
+    
+    
+}
